e987c845c6f9cbce808a0dbdd476570c92e30040
Shaunak Deshpande <86540850+ShaDe2312@users.noreply.github.com>
2023-04-19 09:44:34 +0530

    Add files via upload

---
+++ Merchant/static/script.js
@@ -0,0 +1,42 @@
+console.log("connected")
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+
+  let sender = {
+    'Latitude':position.coords.latitude, 
+    'Longitude':position.coords.longitude,
+  }
+
+  sender = JSON.stringify(sender);
+  console.log(sender)
+  const xhr = new XMLHttpRequest();
+  const URL = '/get-coordinates/' + sender
+
+  xhr.open('POST', URL);
+  xhr.send(sender);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+                var emptyDiv = document.getElementById("emptyDiv")
+                var a = document.createElement('a'); 
+                var link = document.createTextNode("Fill the rest of your profile");
+                a.appendChild(link); 
+                a.title = "Enter information"; 
+                a.href = "http://127.0.0.1:5002/merchant-info"; 
+                emptyDiv.innerHTML = "Your location was recorded successfully!<br><br>";
+                emptyDiv.appendChild(a);
+               
+      // window.location.replace("");
+    }
+  }
+}
---
+++ User/static/script.js
@@ -0,0 +1,48 @@
+console.log("connected")
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+ //JSON Variable
+  let sender = {
+    'Latitude':position.coords.latitude, 
+    'Longitude':position.coords.longitude,
+  }
+
+  sender = JSON.stringify(sender);
+  console.log(sender)
+  const xhr = new XMLHttpRequest(); // to send to server
+  const URL = '/get-coordinates/' + sender
+
+  xhr.open('POST', URL);
+  xhr.send(sender);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      gdata = document.getElementById('garagedata');
+      garageInfo= JSON.parse(xhr.response)
+      console.log(garageInfo)
+      document.getElementById("a1").href = garageInfo["URL"];
+      document.getElementById("p1").innerHTML = "Your nearest garage";
+      data = 'Contact Details are: <br> ' + garageInfo["Name"] + '<br>' + garageInfo['Address']+ '<br>' + garageInfo["Phone"] + '<br>'
+
+      if(garageInfo["Person"]=="Yes"){
+        data += "Vehicle Pickup: Available";
+      }
+      else{
+        data += "Vehicle Pickup: Not Available";
+      }
+
+      gdata.innerHTML= data; 
+
+    }
+  }
+}
